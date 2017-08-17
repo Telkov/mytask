9d5d158145687d0f647092d6a8fb283f4649b571
PR1ME <sanya_fl@ukr.net>
2017-08-17 21:04:15 +0300

    1st end

--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -1,12 +1,35 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: Alejandro
- * Date: 17.08.2017
- * Time: 20:25
- */
 class Router
 {
+    protected static $routes = []; //массив с маршрутами (таблица маршрутов)
+    protected static $route = []; //текущий маршрут по текущему Url
 
+    public static function add($regexp, $route = [])
+    {
+        self::$routes[$regexp] = $route;
+    }
+
+    public static function getRoutes()
+    {
+        return self::$routes;
+    }
+
+    public static function getRoute()
+    {
+        return self::$route;
+    }
+
+    public static function matchRoute($url)
+    {
+        foreach (self::$routes as $pattern => $route) {
+            if($url == $pattern) {
+                self::$route = $route;
+                return true;
+            }
+
+        }
+        return false;
+
+    }
 }
--- vendor/libs/functions.php
+++ vendor/libs/functions.php
@@ -1,7 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Alejandro
- * Date: 17.08.2017
- * Time: 20:38
- */
+
+//красивый вывод массивов для дебага
+function debug($arr) {
+    echo '<pre>' . print_r($arr, true) . '</pre>';
+}
