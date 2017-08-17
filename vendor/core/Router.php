f34f1ce1a14d0afb1f74f49d951847f9e340703c
PR1ME <sanya_fl@ukr.net>
2017-08-17 21:04:07 +0300

    1st end

---
+++ vendor/core/Router.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Alejandro
+ * Date: 17.08.2017
+ * Time: 20:25
+ */
+class Router
+{
+
+}
---
+++ vendor/libs/functions.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alejandro
+ * Date: 17.08.2017
+ * Time: 20:38
+ */
