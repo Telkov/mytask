46afc8d2caf96442b0b0d7b194e8c0ff48e9cb45
PR1ME <sanya_fl@ukr.net>
2017-08-18 16:24:44 +0300

    added App controller, View controller

---
+++ app/controllers/App.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alejandro
+ * Date: 18.08.2017
+ * Time: 15:36
+ */
+
+namespace app\controllers;
+
+
+class App
+{
+
+}
---
+++ vendor/core/base/View.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alejandro
+ * Date: 18.08.2017
+ * Time: 14:58
+ */
+
+namespace vendor\core\base;
+
+
+class View
+{
+
+}
