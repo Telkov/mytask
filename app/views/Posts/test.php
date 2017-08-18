09916c01c5086700d355638aa88d98b617c0d384
PR1ME <sanya_fl@ukr.net>
2017-08-18 11:41:15 +0300

    added

---
+++ app/views/Posts/test.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alejandro
+ * Date: 18.08.2017
+ * Time: 9:58
+ */
---
+++ vendor/core/base/Controller.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alejandro
+ * Date: 18.08.2017
+ * Time: 10:31
+ */
+
+namespace vendor\core\base;
+
+
+class Controller
+{
+
+}
